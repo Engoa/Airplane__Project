c9b4f520c593f263c50f730f631109d79b07d00f
Engoa <idobz1234567890@gmail.com>
2021-09-18 00:36:03 +0300

    fixes

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -35,5 +35,46 @@
     fitty('#fitty-title2', { multiLine: true });
   };
 
+  const apiKey = 'a505effdf68e5b04fa5eb42d16b7374c';
+  const findLocation = () => {
+    const location = navigator.geolocation.getCurrentPosition((success) => {
+      let { latitude, longitude } = success.coords;
+      const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${apiKey}`;
+      fetch(url).then((res) => res.json().then((data) => renderWeatherCards(data)));
+      if (!latitude || !longitude) {
+        console.log('No location selected');
+      }
+    });
+    return location;
+  };
+  findLocation();
+
+  const renderWeatherCards = (data) => {
+    let { temp, weather } = data.current;
+    const { timezone } = data;
+    console.log(data.current);
+    document.querySelector('.render--homecard-weather').innerHTML += `
+      <a href="#Weather" class="card--anchor">
+        <div class="homecards__weather__temperature">
+          <div class="homecards__weather__degrees">
+            <div>
+              <h2 class="city--name" id='fitty-title3'>${timezone}</h2>
+            </div>
+            <div>
+            <h2 class="degree">${temp.toFixed(0)}°</h2>
+          </div>
+            <div>
+              <span class="degree--description">${weather[0].description}</span>
+            </div>
+          </div>
+          <div class="homecards__weather__image">
+            <img src="../assets/images/rainy.png" alt="temperature image" />
+          </div>
+        </div>
+      </a>    
+      `;
+    fitty('#fitty-title3', { multiLine: true });
+
+  };
   drawHomeMediaCard();
 })();
