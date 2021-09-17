3be0add86250525e0828caf2259428791bb9c1bf
Engoa <idobz1234567890@gmail.com>
2021-09-18 02:12:08 +0300

    Revert "merge"
    This reverts commit d88fe6c4009c21f1e74efd1e39d43c82ebd08eef, reversing
    changes made to ff1c6843eabb3c2e02f59df484093fec9587433d.

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -3,14 +3,14 @@
     const drawHomeCard = document.querySelector('.render--homecard-media');
     drawHomeCard.innerHTML = `
     <a href="#Media" class="card--anchor">
-  <div class="homecards__weather__temperature">
+  <div class="homecards__weather__temperature homecards__media">
     <div class="homecards__weather__degrees" id="card-media">
       <div class='mediacard--header--container'>
         <h2 class="degree movie--name" id="fitty-title2">${MediaPlayer.playingNow.title}</h2>
        
       </div>
       <div>
-        <span class="degree--description"
+        <span class="degree--description movie--duration"
           >${MediaPlayer.songs.length} Songs</span
         >
       </div>
@@ -18,7 +18,7 @@
         <i class="fas fa-headphones card--icon"></i>
       </div>
     </div>
-    <div class="homecards__weather__image">
+    <div class="homecards__weather__image homecards__movies--image card--media--img">
       <img src="${MediaPlayer.playingNow.cover_img}" alt="music image" />
        <div class="boxContainer">
           <div class="box box1"></div>
@@ -74,6 +74,7 @@
       </a>    
       `;
     fitty('#fitty-title3', { multiLine: true });
+
   };
   drawHomeMediaCard();
 })();
